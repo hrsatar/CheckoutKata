41cd1f9c4643315e027582e62b2ca123e3cd03cb
hrsatar <hr.sattar@gmail.com>
2024-12-07 17:56:02 +0000

    Add project files.

---
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,17 @@
+namespace CheckoutKata.Tests
+{
+    public class CheckoutTests
+    {
+        private List<PricingRule> Rules;
+
+        public CheckoutTests()
+        {
+            Rules = new List<PricingRule> {
+            new PricingRule {SKU="A",SinglePrice=50,SpecialOfferPrice=130,SpecialOfferQuantity=3 },
+            new PricingRule {SKU="B",SinglePrice=30,SpecialOfferPrice=45,SpecialOfferQuantity=2 },
+            new PricingRule {SKU="C",SinglePrice=20 },
+            new PricingRule {SKU="D",SinglePrice=15 }
+            };
+        }
+    }
+}
