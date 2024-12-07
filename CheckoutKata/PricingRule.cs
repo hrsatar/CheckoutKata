6948dc6bdd566f9f42f74070501f9db849f0b3e9
hrsatar <hr.sattar@gmail.com>
2024-12-07 18:01:05 +0000

    Step 2. Created PricingRule, Created Checkout class library, Moved to PricingRule, Added refrence to Test project

---
+++ CheckoutKata/PricingRule.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckoutKata
+{
+    public class PricingRule
+    {
+        public string SKU { get; set; }
+        public int SinglePrice { get; set; }
+        public int SpecialOfferPrice { get; set; }
+        public int SpecialOfferQuantity { get; set; }
+    }
+}
